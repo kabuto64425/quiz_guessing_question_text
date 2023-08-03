a80dd2ff27d6ebaded9ee343217ad17a79123615
kabuto64425 <higasumi52@yahoo.co.jp>
2023-08-04 01:58:37 +0900

    出題画面、クリックして文字パネルをオープンできるようにする

--- askquestion/urls.py
+++ askquestion/urls.py
@@ -5,5 +5,5 @@ from .views import IndexView
 # アプリケーションのルーティング設定
 
 urlpatterns = [
-    path('<int:deck_pk>/<int:number>', IndexView.as_view(), name='set_question')
+    path('deck_<int:deck_pk>/<int:number>', IndexView.as_view(), name='set_question')
 ]
