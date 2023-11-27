f5c3fe7786cf934044c3ae4cc21fe6c9e05e4414
kabuto64425 <kabuto64425_higasumi52@yahoo.co.jp>
2023-11-27 13:55:27 +0900

    サンプルの名残を削除

--- app/urls.py
+++ app/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
 
-from .models import Item
-from .views import ItemFilterView, ItemDetailView, ItemCreateView, ItemUpdateView, ItemDeleteView, IndexView
+from .views import IndexView
 
 # アプリケーションのルーティング設定
 
 urlpatterns = [
-    path('detail/<int:pk>/', ItemDetailView.as_view(), name='detail'),
-    path('create/', ItemCreateView.as_view(), name='create'),
-    path('update/<int:pk>/', ItemUpdateView.as_view(), name='update'),
-    path('delete/<int:pk>/', ItemDeleteView.as_view(), name='delete'),
     path('', IndexView.as_view(), name='index'),
 ]
