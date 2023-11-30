42d4cae17b01b5bf87ac8845bd31bb3d32a260ef
kabuto64425 <kabuto64425_higasumi52@yahoo.co.jp>
2023-11-30 15:28:25 +0900

    出題画面に表紙と背表紙にジャンプできるボタンを追加

--- askquestion/views.py
+++ askquestion/views.py
@@ -8,6 +8,38 @@ from questioncard.models import QuestionCard
 from deck.models import Deck
 
 
+TOP_QUESTION_NUMBER = -1
+BOTTOM_QUESTION_NUMBER = 2 ** 60
+
+class JumpButtonInfo:
+    def __init__(self, url = None, text = None, is_secondary = False):
+        self.url = url
+        self.text = text
+        self.is_secondary = is_secondary
+
+def build_jump_button_infos(deck_pk, question_cards, question_number):
+    jump_button_infos = []
+
+    jump_button_info = JumpButtonInfo()
+    jump_button_info.url = reverse('ask_question_top', kwargs={'deck_pk': deck_pk})
+    jump_button_info.text = "top"
+    if question_number == TOP_QUESTION_NUMBER : jump_button_info.is_secondary = True
+    jump_button_infos.append(jump_button_info)
+    
+    for i, _ in enumerate(question_cards, start = 1):
+        jump_button_info = JumpButtonInfo()
+        jump_button_info.url = reverse('ask_question', kwargs={'deck_pk': deck_pk, 'number': i})
+        jump_button_info.text = f"{i}"
+        if i == question_number : jump_button_info.is_secondary = True
+        jump_button_infos.append(jump_button_info)
+    
+    jump_button_info = JumpButtonInfo()
+    jump_button_info.url = reverse('ask_question_bottom', kwargs={'deck_pk': deck_pk})
+    jump_button_info.text = "btm"
+    if question_number == BOTTOM_QUESTION_NUMBER : jump_button_info.is_secondary = True
+    jump_button_infos.append(jump_button_info)
+    return jump_button_infos
+
 class TopView(TemplateView):
 
     template_name = "askquestion/top.html"
@@ -37,8 +69,8 @@ class TopView(TemplateView):
         kwargs["next_url"] = reverse('ask_question', kwargs={'deck_pk': self.kwargs.get('deck_pk'), 'number': 1})
         kwargs["prev_url"] = None
 
-        urls = [reverse('ask_question', kwargs={'deck_pk': self.kwargs.get('deck_pk'), 'number': i + 1}) for i in range(len(question_cards))]
-        kwargs["urls"] = urls
+        jump_button_infos = build_jump_button_infos(self.kwargs.get('deck_pk'), question_cards, TOP_QUESTION_NUMBER)
+        kwargs["jump_button_infos"] = jump_button_infos
 
         return super().get_context_data(**kwargs)
 
@@ -71,8 +103,8 @@ class AskQuestionView(TemplateView):
         kwargs["next_url"] = reverse('ask_question', kwargs={'deck_pk': self.kwargs.get('deck_pk'), 'number': self.kwargs.get('number') + 1}) if self.kwargs.get('number') < len(question_cards) else reverse('ask_question_bottom', kwargs={'deck_pk': self.kwargs.get('deck_pk')})
         kwargs["prev_url"] = reverse('ask_question', kwargs={'deck_pk': self.kwargs.get('deck_pk'), 'number': self.kwargs.get('number') - 1}) if self.kwargs.get('number') > 1 else reverse('ask_question_top', kwargs={'deck_pk': self.kwargs.get('deck_pk')})
 
-        urls = [reverse('ask_question', kwargs={'deck_pk': self.kwargs.get('deck_pk'), 'number': i + 1}) for i in range(len(question_cards))]
-        kwargs["urls"] = urls
+        jump_button_infos = build_jump_button_infos(self.kwargs.get('deck_pk'), question_cards, kwargs["question_number"])
+        kwargs["jump_button_infos"] = jump_button_infos
 
         return super().get_context_data(**kwargs)
     
@@ -105,7 +137,7 @@ class BottomView(TemplateView):
         kwargs["next_url"] = None
         kwargs["prev_url"] = reverse('ask_question', kwargs={'deck_pk': self.kwargs.get('deck_pk'), 'number': len(question_cards)})
 
-        urls = [reverse('ask_question', kwargs={'deck_pk': self.kwargs.get('deck_pk'), 'number': i + 1}) for i in range(len(question_cards))]
-        kwargs["urls"] = urls
+        jump_button_infos = build_jump_button_infos(self.kwargs.get('deck_pk'), question_cards, BOTTOM_QUESTION_NUMBER)
+        kwargs["jump_button_infos"] = jump_button_infos
 
         return super().get_context_data(**kwargs)
