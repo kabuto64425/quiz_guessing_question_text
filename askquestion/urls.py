3cee47d8c9cb35e4e89a3f7507e2518a610280ac
kabuto64425 <kabuto64425_higasumi52@yahoo.co.jp>
2023-11-30 14:20:09 +0900

    出題画面、表紙と背表紙を追加

--- askquestion/urls.py
+++ askquestion/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import TopView, AskQuestionView, BottomView
 
 # アプリケーションのルーティング設定
 
 urlpatterns = [
-    path('deck_<int:deck_pk>/<int:number>', IndexView.as_view(), name='ask_question')
+    path('deck_<int:deck_pk>/top', TopView.as_view(), name='ask_question_top'),
+    path('deck_<int:deck_pk>/<int:number>', AskQuestionView.as_view(), name='ask_question'),
+    path('deck_<int:deck_pk>/bottom', BottomView.as_view(), name='ask_question_bottom')
 ]
--- askquestion/views.py
+++ askquestion/views.py
@@ -1,22 +1,52 @@
 from django.shortcuts import render
 #from django.contrib.auth.mixins import LoginRequiredMixin カスタムクラスを使用するため不要
-from django.views.generic import DetailView,TemplateView
+from django.views.generic import TemplateView
 from django.urls import reverse
 from django.core.exceptions import PermissionDenied
-
-from utils.mixins import CustomLoginRequiredMixin #追加
 
 from questioncard.models import QuestionCard
 from deck.models import Deck
 
-# このviewのルーティングは「/myapp/Index」
-class IndexView(TemplateView):
 
-    #model = QuestionCard
+class TopView(TemplateView):
+
+    template_name = "askquestion/top.html"
+
+    # アクセス権がないユーザーのアクセスを防ぐため
+    def get(self, request, *args, **kwargs):
+        deck = Deck.objects.get(pk=self.kwargs.get('deck_pk'))
+        if deck.public_flag or self.request.user == deck.owner:
+            return super().get(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+    
+    def get_context_data(self, **kwargs):
+        """
+        表示データの設定
+        """
+        # 表示データの追加はここで 例：
+        # kwargs['sample'] = 'sample'
+        deck = Deck.objects.get(pk=self.kwargs.get('deck_pk'))
+        question_cards = QuestionCard.objects.filter(in_deck=deck).order_by('order')
+
+        kwargs["owner"] = deck.owner
+        kwargs["dek_name"] = deck.deck_name
+
+        kwargs["question_number"] = None
+
+        kwargs["next_url"] = reverse('ask_question', kwargs={'deck_pk': self.kwargs.get('deck_pk'), 'number': 1})
+        kwargs["prev_url"] = None
+
+        urls = [reverse('ask_question', kwargs={'deck_pk': self.kwargs.get('deck_pk'), 'number': i + 1}) for i in range(len(question_cards))]
+        kwargs["urls"] = urls
+
+        return super().get_context_data(**kwargs)
+
+class AskQuestionView(TemplateView):
 
     template_name = "askquestion/ask_question.html"
 
-    # 自身のデッキに対してほかユーザーがアクセスするのを防ぐため
+    # アクセス権がないユーザーのアクセスを防ぐため
     def get(self, request, *args, **kwargs):
         deck = Deck.objects.get(pk=self.kwargs.get('deck_pk'))
         if deck.public_flag or self.request.user == deck.owner:
@@ -38,8 +68,42 @@ class IndexView(TemplateView):
 
         kwargs["question_number"] = self.kwargs.get('number')
 
-        kwargs["next_url"] = reverse('ask_question', kwargs={'deck_pk': self.kwargs.get('deck_pk'), 'number': self.kwargs.get('number') + 1}) if self.kwargs.get('number') < len(question_cards) else None
-        kwargs["prev_url"] = reverse('ask_question', kwargs={'deck_pk': self.kwargs.get('deck_pk'), 'number': self.kwargs.get('number') - 1}) if self.kwargs.get('number') > 1 else None
+        kwargs["next_url"] = reverse('ask_question', kwargs={'deck_pk': self.kwargs.get('deck_pk'), 'number': self.kwargs.get('number') + 1}) if self.kwargs.get('number') < len(question_cards) else reverse('ask_question_bottom', kwargs={'deck_pk': self.kwargs.get('deck_pk')})
+        kwargs["prev_url"] = reverse('ask_question', kwargs={'deck_pk': self.kwargs.get('deck_pk'), 'number': self.kwargs.get('number') - 1}) if self.kwargs.get('number') > 1 else reverse('ask_question_top', kwargs={'deck_pk': self.kwargs.get('deck_pk')})
+
+        urls = [reverse('ask_question', kwargs={'deck_pk': self.kwargs.get('deck_pk'), 'number': i + 1}) for i in range(len(question_cards))]
+        kwargs["urls"] = urls
+
+        return super().get_context_data(**kwargs)
+    
+class BottomView(TemplateView):
+
+    template_name = "askquestion/bottom.html"
+
+    # アクセス権がないユーザーのアクセスを防ぐため
+    def get(self, request, *args, **kwargs):
+        deck = Deck.objects.get(pk=self.kwargs.get('deck_pk'))
+        if deck.public_flag or self.request.user == deck.owner:
+            return super().get(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+    
+    def get_context_data(self, **kwargs):
+        """
+        表示データの設定
+        """
+        # 表示データの追加はここで 例：
+        # kwargs['sample'] = 'sample'
+        deck = Deck.objects.get(pk=self.kwargs.get('deck_pk'))
+        question_cards = QuestionCard.objects.filter(in_deck=deck).order_by('order')
+
+        kwargs["owner"] = deck.owner
+        kwargs["dek_name"] = deck.deck_name
+
+        kwargs["question_number"] = None
+
+        kwargs["next_url"] = None
+        kwargs["prev_url"] = reverse('ask_question', kwargs={'deck_pk': self.kwargs.get('deck_pk'), 'number': len(question_cards)})
 
         urls = [reverse('ask_question', kwargs={'deck_pk': self.kwargs.get('deck_pk'), 'number': i + 1}) for i in range(len(question_cards))]
         kwargs["urls"] = urls
